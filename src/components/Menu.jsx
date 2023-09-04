fde273d7100005a9a85ee77f1c3117d78abe6fd9
Caliche Orozco <caliche2da1@gmail.com>
2023-09-04 11:10:40 -0600

    future: Instructions and styles

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useStore } from "../hooks/useStore";
 
 export const Menu = () => {
@@ -6,10 +7,29 @@ export const Menu = () => {
     state.resetWorld,
   ]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      document.querySelector(".instructions").style.display = "none";
+    }, 15000);
+
+    return () => {
+      clearInterval();
+    };
+  }, []);
+
   return (
-    <div className="menu">
-      <button onClick={() => saveWorld()}>Save</button>
-      <button onClick={() => resetWorld()}>Reset</button>
-    </div>
+    <>
+      <div className="menu">
+        <button onClick={() => saveWorld()}>Save</button>
+        <button onClick={() => resetWorld()}>Reset</button>
+      </div>
+      <div className="instructions">
+        <h2>Instructions</h2>
+        <span>W A S D to move</span>
+        <span>Space to jump</span>
+        <span>Click to add cube</span>
+        <span>Alt + click to remove</span>
+      </div>
+    </>
   );
 };
